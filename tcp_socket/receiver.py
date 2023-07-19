3b08b0cdaf2f4bd16dc0f3cc5a155c6645ede02e
namch <namch@kalapa.vn>
2023-07-19 10:05:49 +0700

    initial commit: tcp socket

---
+++ tcp_socket/receiver.py
@@ -0,0 +1,32 @@
+import socket
+
+# Create a TCP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to a specific address and port
+host = '202.134.19.49'  # Change this to the desired host address
+port = 1606  # Change this to the desired port number
+s.bind((host, port))
+
+# Listen for incoming connections
+s.listen(1)
+
+print("Receiver is ready to receive data.")
+
+# Accept a client connection
+while True:
+    conn, addr = s.accept()
+    print("Connected by:", addr)
+
+    # Receive and print data continuously
+    while True:
+        # conn, addr = s.accept()
+        # print("Connected by:", addr) 
+        data = conn.recv(1024)
+        if not data:
+            break
+        print("Received data:", data.decode())
+
+    # Close the connection
+    conn.close()
+    print("Connection closed.")
